2e3f42336b024f0d02f7943f51c47c430fb239ea
Jonas Dahlbæk <30782351+dahlbaek@users.noreply.github.com>
2024-12-11 22:11:58 +0100

    Day 11 part 2

--- day11/src/main.rs
+++ day11/src/main.rs
@@ -1,8 +1,9 @@
 use core::str;
+use std::collections::{HashMap, HashSet};
 
 const PUZZLE: &str = include_str!("puzzle");
 
-fn count_part1(i: u64, left: u8) -> u64 {
+fn count_part1(i: u64, left: u32) -> u64 {
     if left == 0 {
         1
     } else if i == 0 {
@@ -19,6 +20,57 @@ fn count_part1(i: u64, left: u8) -> u64 {
     }
 }
 
+fn next_elements(element: u64) -> Vec<u64> {
+    if element == 0 {
+        vec![1]
+    } else {
+        let element_string = element.to_string();
+        let element_len = element_string.len();
+        if element_len % 2 == 0 {
+            vec![
+                element_string[..element_len / 2].parse().unwrap(),
+                element_string[element_len / 2..].parse().unwrap(),
+            ]
+        } else {
+            vec![element * 2024]
+        }
+    }
+}
+
+fn add_elements(mut space: HashSet<u64>, &element: &u64) -> HashSet<u64> {
+    let mut elems = vec![element];
+    while !elems.is_empty() {
+        for &elem in &elems {
+            space.insert(elem);
+        }
+        elems = elems
+            .iter()
+            .flat_map(|&elem| next_elements(elem))
+            .filter(|elem| !space.contains(elem))
+            .collect::<Vec<_>>();
+    }
+    space
+}
+
+fn next_counts(
+    acc: HashMap<(u64, u32), u64>,
+    left: u32,
+    space: &HashSet<u64>,
+) -> HashMap<(u64, u32), u64> {
+    space.iter().fold(HashMap::new(), |mut next_acc, &i| {
+        let count = if left == 0 {
+            1
+        } else {
+            next_elements(i)
+                .iter()
+                .map(|&next_element| acc[&(next_element, left - 1)])
+                .sum::<u64>()
+        };
+        next_acc.insert((i, left), count);
+        next_acc
+    })
+}
+
 fn main() {
     let parsed = PUZZLE
         .split_whitespace()
@@ -28,6 +80,8 @@ fn main() {
     let part1 = parsed.iter().map(|&i| count_part1(i, 25)).sum::<u64>();
     println!("Part 1: {}", part1);
 
-    let part2 = parsed.iter().map(|&i| count_part1(i, 75)).sum::<u64>();
+    let space = parsed.iter().fold(HashSet::new(), add_elements);
+    let counts = (0..76).fold(HashMap::new(), |acc, left| next_counts(acc, left, &space));
+    let part2 = parsed.iter().map(|&i| counts[&(i, 75)]).sum::<u64>();
     println!("Part 2: {}", part2);
 }
