128cedd52ecac5de82134876f1735bd986a213cf
Jonas Dahlbæk <30782351+dahlbaek@users.noreply.github.com>
2024-12-22 00:12:14 +0100

    Use cache for day 19
    The cache is much faster for small input strings, whereas
    the split-in-half solution will avoid blowing the stack
    for any reasonable input size. The cache solution is
    simpler to implement and probably more reuseable.

--- day19/src/main.rs
+++ day19/src/main.rs
@@ -1,4 +1,8 @@
-use std::collections::HashSet;
+use std::{
+    borrow::Borrow,
+    collections::{HashMap, HashSet},
+    hash::Hash,
+};
 
 const PUZZLE: &str = include_str!("puzzle");
 
@@ -10,55 +14,60 @@ fn parse() -> (HashSet<&'static str>, Vec<&'static str>) {
     (available, targets)
 }
 
-fn is_buildable_small(available: &HashSet<&str>, target: &str) -> bool {
+fn is_buildable<'a>(
+    cache: &mut Cache<String, bool, str, HashSet<&'a str>>,
+    target: &str,
+    available: &HashSet<&'a str>,
+) -> bool {
     target.is_empty()
-        || (1..=target.len()).any(|j| {
-            available.contains(&target[..j]) && is_buildable_small(available, &target[j..])
-        })
+        || (1..=target.len())
+            .filter(|&j| available.contains(&target[..j]))
+            .any(|j| cache.get_or_compute(&target[j..], available))
 }
 
-fn is_buildable(available: &HashSet<&str>, target: &str) -> bool {
-    if target.len() < 16 {
-        is_buildable_small(available, target)
-    } else {
-        available.iter().any(|s| {
-            let midsection_start = target.len() / 2 - 4;
-            target[midsection_start..target.len() / 2 + 4]
-                .find(s)
-                .is_some_and(|index| {
-                    is_buildable(available, &target[..midsection_start + index])
-                        && is_buildable(available, &target[midsection_start + index + s.len()..])
-                })
-        })
-    }
-}
-
-fn count_small(available: &HashSet<&str>, target: &str) -> u64 {
+fn count<'a>(
+    cache: &mut Cache<String, u64, str, HashSet<&'a str>>,
+    target: &str,
+    available: &HashSet<&'a str>,
+) -> u64 {
     if target.is_empty() {
         1
     } else {
         (1..=target.len())
             .filter(|&j| available.contains(&target[..j]))
-            .map(|j| count_small(available, &target[j..]))
+            .map(|j| cache.get_or_compute(&target[j..], available))
             .sum::<u64>()
     }
 }
 
-fn count(available: &HashSet<&str>, target: &str) -> u64 {
-    if target.len() < 16 {
-        count_small(available, target)
-    } else {
-        available
-            .iter()
-            .flat_map(|&s| {
-                let midsection_start = target.len() / 2 - s.len() + 1;
-                (midsection_start..=target.len() / 2)
-                    .filter(move |&j| &target[j..j + s.len()] == s)
-                    .map(move |j| {
-                        count(available, &target[..j]) * count(available, &target[j + s.len()..])
-                    })
-            })
-            .sum()
+struct Cache<K, V, Q: ?Sized, C> {
+    inner: HashMap<K, V>,
+    f: fn(&mut Self, &Q, &C) -> V,
+}
+
+impl<K, V, Q, C> Cache<K, V, Q, C>
+where
+    Q: ?Sized,
+    Q: Eq + Hash,
+    K: Eq + Hash,
+    Q: ToOwned<Owned = K>,
+    K: Borrow<Q>,
+    V: Clone,
+{
+    fn new(f: fn(&mut Self, &Q, &C) -> V) -> Cache<K, V, Q, C> {
+        let inner = HashMap::new();
+        Cache { inner, f }
+    }
+
+    fn get_or_compute(&mut self, q: &Q, c: &C) -> V {
+        match self.inner.get(q) {
+            Some(v) => v.clone(),
+            None => {
+                let v = (self.f)(self, q, c);
+                self.inner.insert(q.to_owned(), v.clone());
+                v
+            }
+        }
     }
 }
 
@@ -67,13 +76,13 @@ fn main() {
 
     let part1 = targets
         .iter()
-        .filter(|target| is_buildable(&available, target))
+        .filter(|target| Cache::new(is_buildable).get_or_compute(target, &available))
         .count();
     println!("Part 1: {}", part1);
 
     let part2 = targets
         .iter()
-        .map(|target| count(&available, target))
+        .map(|target| Cache::new(count).get_or_compute(target, &available))
         .sum::<u64>();
     println!("Part 2: {}", part2);
 }
